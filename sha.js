de3e20093eb0d6ffde7176e9d0fa4227162eb2f7
Prez Ivan <ivan.camtv@gmail.com>
2020-04-25 15:22:58 +0200

    parameter and autocopy

--- sha.js
+++ sha.js
@@ -18,3 +18,30 @@ function SHA(Algorithm,str) {
 }
 
 window.SHA = SHA;
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+    }
+    return urlparameter;
+}
+
+$(document).ready(function(){
+
+    var param = getUrlParam("prefix","");
+    navigator.clipboard.readText()
+        .then(text => {
+            $("#TXT").val(param+text);
+        })
+        .catch(err => {
+        });
+})
